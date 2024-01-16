1ce28fa5b7899f13856bcb5aa3ce767b107228df
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-01-16 15:14:13 -0800

    pushing code

--- src/com/java/LambdaExpression.java
+++ src/com/java/LambdaExpression.java
@@ -5,17 +5,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class LambdaExpression {
-public static void main(String arg[]) {
-	List <Integer>list=new ArrayList<>();
-	list.add(2);
-	list.add(4);
-	list.add(65);
-	list.add(43);
-	Collections.sort(list,(a, b)->a-b);
-	System.out.println(list);
-
-	
-	
-}
+	public static void main(String arg[]) {
+		List<Integer> list = new ArrayList<>();
+		list.add(2);
+		list.add(4);
+		list.add(65);
+		list.add(43);
+		Collections.sort(list, (a, b) -> a - b);
+		System.out.println(list);
+	}
 
 }
