4c0c9ed1c4c7e3868062408894455536c07f5ac6
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-01-22 11:21:24 -0800

    pushhing code

--- src/com/collections/Student2.java
+++ src/com/collections/Student2.java
@@ -4,23 +4,23 @@ public class Student2 {
 	int RollNo;
 	String Name;
 	String College = "ITS";
-Student2(int R,String n ){
-	RollNo = R;
-	Name = n;
-}
- void display() {
- System.out.println(RollNo+ " "+Name+ " "+College);
- }
+
+	Student2(int R, String n) {
+		RollNo = R;
+		Name = n;
+	}
+
+	void display() {
+		System.out.println(RollNo + " " + Name + " " + College);
+	}
 
 	public static void main(String[] args) {
-		Student2 s1 = new
-				Student2(111,"Karan");
-				
-				Student2 s2 = new
-				Student2(222,"Aryan");
-				s1.display();
-				s2.display();
-				
+		Student2 s1 = new Student2(111, "Karan");
+
+		Student2 s2 = new Student2(222, "Aryan");
+		s1.display();
+		s2.display();
+
 	}
 
 }
