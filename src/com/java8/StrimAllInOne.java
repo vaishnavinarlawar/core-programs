0a744ce8be6f772a307d85cdef1fd09ce2a63999
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-01-25 11:37:23 -0800

    pushing code

--- src/com/java8/StrimAllInOne.java
+++ src/com/java8/StrimAllInOne.java
@@ -8,25 +8,41 @@ public class StrimAllInOne {
 
 	public static void main(String[] args) {
 
-		int a[] = { 1, 2, 3, 4, 2, 6, 5, 7, 7, 3 };
-		Map<Integer, Integer> hm = new HashMap<>();
-		for (int b : a) {
-			if (hm.containsKey(b)) {
-				hm.put(b, hm.get(b) + 1);
-			}
+//		int a[] = { 1, 2, 3, 4, 2, 6, 5, 7, 7, 3 };
+//		Map<Integer, Integer> hm = new HashMap<>();
+//		for (int b : a) {
+//			if (hm.containsKey(b)) {
+//				hm.put(b, hm.get(b) + 1);
+//			}
+//
+//			else {
+//				hm.put(b, 1);
+//
+//			}
+//
+//		}
+//		for (Entry<Integer, Integer> h : hm.entrySet()) {
+//			if (h.getValue() == 1) {
+//				System.out.println(h.getKey() + "");
+//			}
+//		}
 
+		int a[]= {1,2,3,4,1,4,7,3,3,3,6};
+		Map<Integer,Integer>hm=new HashMap<>();
+		for(int b:a) {
+			if(hm.containsKey(b)) {
+				hm.put(b,hm.get(b)+1 );
+			}
 			else {
 				hm.put(b, 1);
-
-			}
-
-		}
-		for (Entry<Integer, Integer> h : hm.entrySet()) {
-			if (h.getValue() == 1) {
-				System.out.println(h.getKey() + "");
 			}
 		}
-
+		
+		for(Entry<Integer, Integer>h:hm.entrySet()) {
+			if(h.getValue()==2) {
+				System.out.println(h.getKey()+" ");
+			}
+		}
 	}
 
 }
