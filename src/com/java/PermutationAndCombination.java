0d65416365a3e9bc54927e8b0332bbacf73cfc64
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-01-27 11:20:53 -0800

    pushing code

--- src/com/java/PermutationAndCombination.java
+++ src/com/java/PermutationAndCombination.java
@@ -4,26 +4,24 @@ import java.util.Scanner;
 public class PermutationAndCombination {
 
 	public static int fact(int num) {
-		
-		int fact=1, i;
-		for(i=1; i<=num; i++) {
-			fact = fact*i;
-			}
-			return fact;
+
+		int fact = 1, i;
+		for (i = 1; i <= num; i++) {
+			fact = fact * i;
 		}
-		public static void main(String[] args)
-		{
-			int n,r;
-			Scanner scan = new Scanner(System.in);
-			System.out.print("Enter the value of n: ");
-			n = scan.nextInt();
-			System.out.println("Enter the value of r: ");
-			r = scan.nextInt();
-			System.out.print("NCR = " +(fact(n)/(fact(n-r)*fact(r))));
-			System.out.print("nNPR = "+(fact(n)/(fact(n-r))));
-			
-		}
-			
+		return fact;
 	}
 
+	public static void main(String[] args) {
+		int n, r;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the value of n: ");
+		n = scan.nextInt();
+		System.out.println("Enter the value of r: ");
+		r = scan.nextInt();
+		System.out.print("NCR = " + (fact(n) / (fact(n - r) * fact(r))));
+		System.out.print("nNPR = " + (fact(n) / (fact(n - r))));
 
+	}
+
+}
