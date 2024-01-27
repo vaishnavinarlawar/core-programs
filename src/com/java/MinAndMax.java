84cf6585dc56c55697439824a8e543f04bc827ac
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-01-27 11:28:34 -0800

    pushing code

--- src/com/java/MinAndMax.java
+++ src/com/java/MinAndMax.java
@@ -3,24 +3,23 @@ package com.java;
 public class MinAndMax {
 
 	public static void main(String[] args) {
-		int[] arr= {6,-2,3,5,6,7,8,9,10};
-		
-        int max =0, min=arr[0];
-		
-		for(int i=1; i<arr.length; i++) {
-			if(arr[i]>max) {
-				max=arr[i];
+		int[] arr = { 6, -2, 3, 5, 6, 7, 8, 9, 10 };
+
+		int max = 0, min = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
 			}
-				if(arr[i]<min) {
-					min=arr[i];
-					
-				}
+			if (arr[i] < min) {
+				min = arr[i];
+
 			}
-		
-		
-		System.out.println("minimum value: "+max);
-		
-		System.out.println("minimum value: "+min);
+		}
+
+		System.out.println("minimum value: " + max);
+
+		System.out.println("minimum value: " + min);
 	}
-	
+
 }
