092f887f9b31b428fc9e4a5f2849a1ba619e0957
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-01-27 11:24:17 -0800

    pushing another code

--- src/com/java/Sort.java
+++ src/com/java/Sort.java
@@ -4,23 +4,20 @@ import java.util.*;
 public class Sort {
 
 	public static void main(String[] args) {
-		int[]arr = {10,7,6,2,1,5};
-		int temp=0;
-		int l=arr.length;
+		int[] arr = { 10, 7, 6, 2, 1, 5 };
+		int temp = 0;
+		int l = arr.length;
 
-				
-		for(int i=0; i<=l-1; i++) 
-		{
-			
-			for(int j=i+1; j<=l-1; j++)
-			{
-				if(arr[i]>arr[j]) {
-					temp=arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
+		for (int i = 0; i <= l - 1; i++) {
+
+			for (int j = i + 1; j <= l - 1; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
-			
+
 		}
 		System.out.println(Arrays.toString(arr));
 	}
