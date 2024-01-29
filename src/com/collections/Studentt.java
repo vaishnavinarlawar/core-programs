a684c5583d664a329c5037400143b15fc2013a40
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-01-29 12:19:12 -0800

    pushing code

--- src/com/collections/Studentt.java
+++ src/com/collections/Studentt.java
@@ -5,34 +5,28 @@ public class Studentt {
 	String Student_Name;
 	String Department_Name;
 	String College_Name;
-	
-	Studentt(int SR, String SN, String DN, String CN)
-	{ 
-	Student_RollNo = SR;
-	Student_Name = SN;
-	Department_Name = DN;
-	College_Name = CN;
 
-	
+	Studentt(int SR, String SN, String DN, String CN) {
+		Student_RollNo = SR;
+		Student_Name = SN;
+		Department_Name = DN;
+		College_Name = CN;
+
 	}
-	void Display()
-	{
-		System.out.println(Student_RollNo+ " " +Student_Name+ " " +Department_Name);
+
+	void Display() {
+		System.out.println(Student_RollNo + " " + Student_Name + " " + Department_Name);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("***Student Details***");
-		 Studentt s1 = new  Studentt(101, "Vaishnavi", "Computer Departmment","JSPM College Pune");
-		 Studentt s2 = new  Studentt(102, "Ravi", "Mechanical Departmment","Sinhgad College Pune");
-		 Studentt s3 = new  Studentt(103, "Ganesh", "civil Departmment","PICT College Pune");
-		 s1.Display();
-		 s2.Display();
-		 s3.Display();
-		 
-	 
-		
-	
-		
+		Studentt s1 = new Studentt(101, "Vaishnavi", "Computer Departmment", "JSPM College Pune");
+		Studentt s2 = new Studentt(102, "Ravi", "Mechanical Departmment", "Sinhgad College Pune");
+		Studentt s3 = new Studentt(103, "Ganesh", "civil Departmment", "PICT College Pune");
+		s1.Display();
+		s2.Display();
+		s3.Display();
+
 	}
 
 }
