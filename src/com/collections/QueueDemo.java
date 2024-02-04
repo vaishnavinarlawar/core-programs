c1caede6a13497ea1cedb5f71b702631e2e5ea7b
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-02-04 11:45:05 -0800

    pushing code

--- src/com/collections/QueueDemo.java
+++ src/com/collections/QueueDemo.java
@@ -6,23 +6,23 @@ import java.util.Queue;
 public class QueueDemo {
 
 	public static void main(String[] args) {
-		Queue<String>languages = new LinkedList<>();
+		Queue<String> languages = new LinkedList<>();
 
 		languages.add("java");
 		languages.add("python");
 		languages.add("c");
 		languages.add("c++");
 
-		//access first element
+		// access first element
 		String l1 = languages.peek();
 		System.out.println(l1);
 
-		//access the first element and remove  
+		// access the first element and remove
 		String l2 = languages.poll();
-		System.out.println("remove element "+l2);
+		System.out.println("remove element " + l2);
 
 		System.out.println(languages);
-		//add element at the end //FIFO
+		// add element at the end //FIFO
 
 		languages.offer("Golang");
 		System.out.println(languages);
@@ -33,7 +33,6 @@ public class QueueDemo {
 
 		languages.add("HTML");
 		System.out.println(languages);
-
 
 	}
 
