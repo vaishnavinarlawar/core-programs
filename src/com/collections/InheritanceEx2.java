6c8dd6ada068149dda8d4e91b65f832831d78bd7
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-02-22 16:22:36 -0800

    pushing code

--- src/com/collections/InheritanceEx2.java
+++ src/com/collections/InheritanceEx2.java
@@ -1,41 +1,44 @@
 package com.collections;
 
 class SolarSystem1 {
-public void s() {
-	System.out.println("This is SolarSystem1");
+	public void s() {
+		System.out.println("This is SolarSystem1");
+	}
 }
- }
- class Earth extends SolarSystem1{
+
+class Earth extends SolarSystem1 {
 	public void a() {
 		System.out.println("This Is Earth");
 	}
-	 
- }
- class Mars extends SolarSystem1{
-	 public void b() {
-			System.out.println("This Is Mars");
-		}
-	 
- }
- class Moon extends Earth{
-	 
-	 public void d() {
-			System.out.println("This Is Moon");
-		}
- }
-  
+
+}
+
+class Mars extends SolarSystem1 {
+	public void b() {
+		System.out.println("This Is Mars");
+	}
+
+}
+
+class Moon extends Earth {
+
+	public void d() {
+		System.out.println("This Is Moon");
+	}
+}
+
 public class InheritanceEx2 {
 
 	public static void main(String[] args) {
-		
+
 		Earth a = new Earth();
 		Mars b = new Mars();
 		Moon d = new Moon();
-	
+
 		a.a();
 		b.b();
 		d.d();
-		
+
 	}
 
 }
