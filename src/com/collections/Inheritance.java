8eb0311179ed886871e6757a9f569fc65cef3b9b
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-02-25 20:23:33 -0800

    pushing code

--- src/com/collections/Inheritance.java
+++ src/com/collections/Inheritance.java
@@ -7,28 +7,30 @@ public class Inheritance {
 		obj1.m();
 		obj1.m1();
 	}
-	}
-		 class C {
+}
 
-				
-				void m() {
-					System.out.println("m");
-				 }
-			 }
-		          class c1 extends C{
-					void m1() {
-						System.out.println("m1");
-				}
-		          }
-					
-					 class c2 extends C{
-							void m2() {
-								System.out.println("m2");
-						}
-					 }
-							 class c3 extends C{
-									void m3() {
-										System.out.println("m3");
+class C {
+
+	void m() {
+		System.out.println("m");
+	}
+}
+
+class c1 extends C {
+	void m1() {
+		System.out.println("m1");
+	}
+}
+
+class c2 extends C {
+	void m2() {
+		System.out.println("m2");
+	}
+}
+
+class c3 extends C {
+	void m3() {
+		System.out.println("m3");
 	}
 
 }
