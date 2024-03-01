603557c719f11a0f87b9dde301b59260c218d616
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-02-29 21:44:37 -0800

    pushing code

--- src/com/collections/ArrayDeque1.java
+++ src/com/collections/ArrayDeque1.java
@@ -3,26 +3,24 @@ package com.collections;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-
 public class ArrayDeque1 {
 
 	public static void main(String[] args) {
-		Deque<String>d1=new ArrayDeque<>();
-        d1.push("Message1");
-        d1.push("Message2");
-        
-        System.out.println(d1);
-        
-        System.out.println(d1.pop());
-        
-        System.out.println(d1.peek());
-        
-        System.out.println(d1.poll());
-        
-        System.out.println(d1.pollLast());
-        
-        System.out.println(d1.peekFirst());
+		Deque<String> d1 = new ArrayDeque<>();
+		d1.push("Message1");
+		d1.push("Message2");
 
+		System.out.println(d1);
+
+		System.out.println(d1.pop());
+
+		System.out.println(d1.peek());
+
+		System.out.println(d1.poll());
+
+		System.out.println(d1.pollLast());
+
+		System.out.println(d1.peekFirst());
 
 	}
 
