36d66f35d750962f4ccd47be96b0eb68a31424af
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-03-03 21:29:55 -0800

    pushing overloading code

--- src/com/collections/MethodOverRiding.java
+++ src/com/collections/MethodOverRiding.java
@@ -1,25 +1,25 @@
 package com.collections;
 
 public class MethodOverRiding {
-	
-		public static class Animal{
-		
+
+	public static class Animal {
+
 		public String bark() {
 			return "Dont Know";
 		}
 	}
-	public static  class Cat extends MethodOverRiding  {
-		
+
+	public static class Cat extends MethodOverRiding {
+
 		public String bark() {
 			return "Meow Meow";
 		}
 	}
-
 
 	public static void main(String[] args) {
 		Cat cat = new Cat();
 		System.out.println(cat.bark());
 
 	}
-	
-	}
+
+}
